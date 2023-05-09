7d4accdd81f1dfc99c3504a6357752896df5a47f
Abdul Jaweed <jdgaming7320@gmail.com>
2023-05-09 15:05:25 +0530

    README.md file updated

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 import setuptools
 
-# with open("README.md", "r", encoding="utf-8") as f:
-#     long_description = f.read()
-    
-__version__="0.0.1"
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+__version__ = "0.0.1"
 
 REPO_NAME = "mlfast"
 AUTHOR_USER_NAME = "Abdul-Jaweed"
@@ -17,12 +17,12 @@ setuptools.setup(
     author=AUTHOR_USER_NAME,
     author_email=AUTHOR_EMAIL,
     description=DESCRIPTION,
-    #long_description=long_description,
-    #long_description_content = "text/markdown",
-    url = f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     project_urls={
         "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
     },
     package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src")
+    packages=setuptools.find_packages(where="src"),
 )
