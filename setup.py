4fa746a89f47bde7d375c932368c1aa2e070db66
Abdul Jaweed <jdgaming7320@gmail.com>
2023-05-09 15:19:30 +0530

    README.md file updated

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 
-with open("README.md", "r", encoding="utf-8") as f:
-    long_description = f.read()
+# with open("README.md", "r", encoding="utf-8") as f:
+#     long_description = f.read()
 
 __version__ = "0.0.1"
 
@@ -17,8 +17,8 @@ setuptools.setup(
     author=AUTHOR_USER_NAME,
     author_email=AUTHOR_EMAIL,
     description=DESCRIPTION,
-    long_description=long_description,
-    long_description_content="text/markdown",
+    # long_description=long_description,
+    # long_description_content="text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     project_urls={
         "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
