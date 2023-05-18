38f57010bcd79567325e00564a4cf7cda8a3a409
Abdul Jaweed <jdgaming7320@gmail.com>
2023-05-18 20:59:20 +0530

    bugs fixed to new version v0.0.7

--- src/mlfast/Text_preprocessing.py
+++ src/mlfast/Text_preprocessing.py
@@ -9,7 +9,6 @@ nltk.download('wordnet')
 from nltk.tokenize import word_tokenize
 from nltk.stem import PorterStemmer, WordNetLemmatizer
 import re
-import emoji
 
 
 def Text_preprocessing(
@@ -32,10 +31,29 @@ def Text_preprocessing(
         url_pattern = re.compile(r"https?://\S+|www\.\S+")
         text = url_pattern.sub("", text)
 
+    # # Removing emojis
+    # if remove_emoji:
+    #     text = emoji.demojize(text)
+    #     text = re.sub(r":[a-zA-Z_]+:", "", text)
+        
+    
     # Removing emojis
     if remove_emoji:
-        text = emoji.demojize(text)
-        text = re.sub(r":[a-zA-Z_]+:", "", text)
+        emoji_pattern = re.compile("["
+                               u"\U0001F600-\U0001F64F"  # emoticons
+                               u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                               u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                               u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                               u"\U00002702-\U000027B0"  # other miscellaneous symbols
+                               u"\U000024C2-\U0001F251" 
+                               "]+", flags=re.UNICODE)
+    text = emoji_pattern.sub(r"", text)
+    text = re.sub(r":[a-zA-Z_]+:", "", text)  # Remove emoji names within colons
+    text = re.sub(r"\s+", " ", text)  # Remove extra spaces
+    
+    
+    
+    
 
     # Removing special characters
     if remove_special_chars:
